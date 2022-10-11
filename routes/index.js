5fffbe01dba31099720d065841a5a381aa6fc0b5
uhfm <uhfm@fresnou.org>
2022-10-11 11:39:03 -0700

    commit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', );
+});
+
+router.post('/card', function(req, res){
+  console.log(req.body.dateOfBirth)
+  res.render('card', {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    type: req.body.type,
+    dateOfBirth: new Date(req.body.dateOfBirth),
+    addressLine1: req.body.addressLine1,
+    addressLine1: req.body.addressLine1,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    accountNumber: 12345,
+    currentDate: new Date()
+  })
+})
+
+module.exports = router;
